ebcdb9e2659ea7c48916c6070183b74711d7c5c3
finnr <finnribbeck@gmail.com>
2021-03-07 12:51:09 +0100

    Added the BoardController class.

---
+++ BoardController.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class BoardController
+{
+	public:
+
+		// Creates a new BoardController instance, which is used to make changes to the game instance and to update the View.
+		BoardController();
+
+		// Deletes the BoardController instance and frees the memory allocated by this instance.
+		~BoardController();
+
+
+};
+
