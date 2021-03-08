bcbdc5e70c6f7b47498845a9b60d9f7a0278fa6e
finnr <finnribbeck@gmail.com>
2021-03-08 11:56:39 +0100

    Added the color enum class.

---
+++ Color.h
@@ -0,0 +1,22 @@
+#pragma once
+
+/// <summary>
+/// An enum class for the color of the pieces and tiles.
+/// </summary>
+enum class Color
+{
+	/// <summary>
+	/// The color of the white pieces and tiles.
+	/// </summary>
+	WHITE = 0,
+
+	/// <summary>
+	/// The color of the black pieces and tiles.
+	/// </summary>
+	BLACK,
+
+	/// <summary>
+	/// Used to describe no color, meaning an empty tile or no tile at all.
+	/// </summary>
+	NONE
+};
